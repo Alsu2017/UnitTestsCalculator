ed512c7b8d8669878053a6acb155ebef1bf827e6
Alsu <alsu2017@gmail.com>
2024-12-03 23:52:28 +0300

    25-homework-simpleAPI

---
+++ src/test/java/SeleniumWebStorageShadowDomTests.java
@@ -0,0 +1,64 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.html5.LocalStorage;
+import org.openqa.selenium.html5.SessionStorage;
+import org.openqa.selenium.html5.WebStorage;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.example.Constants.BASE_URL;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class SeleniumWebStorageTests {
+    WebDriver driver;
+
+    @BeforeEach
+    void start() {
+        driver = new ChromeDriver();
+        driver.get(BASE_URL);
+    }
+
+    @AfterEach
+    void close() {
+        driver.quit();
+    }
+
+    @Test
+    void testWebStorage() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-storage.html");
+        WebStorage webStorage = (WebStorage) driver;
+
+        LocalStorage localStorage = webStorage.getLocalStorage();
+        System.out.printf("Local storage elements: {%s}\n", localStorage.size());
+
+        SessionStorage sessionStorage = webStorage.getSessionStorage();
+        sessionStorage.keySet()
+                .forEach(key -> System.out.printf("Session storage: {%s}={%s}\n", key, sessionStorage.getItem(key)));
+        assertThat(sessionStorage.size()).isEqualTo(2);
+
+        sessionStorage.setItem("new element", "new value");
+        assertThat(sessionStorage.size()).isEqualTo(3);
+
+        driver.findElement(By.id("display-session")).click();
+    }
+
+    @Test
+    void testShadowDom() {
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/shadow-dom.html");
+
+        assertThrows(NoSuchElementException.class, () -> driver.findElement(By.cssSelector("p")));
+        WebElement content = driver.findElement(By.id("content"));
+        SearchContext shadowRoot = content.getShadowRoot();
+        WebElement textElement = shadowRoot.findElement(By.cssSelector("p"));
+        assertThat(textElement.getText()).contains("Hello Shadow DOM");
+    }
+}
